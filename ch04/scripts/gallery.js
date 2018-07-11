57637fca2e7b3d20026bdd640d4b656a9361303d
sepiggy <jms1209@qq.com>
2018-07-11 15:50:03 +0800

    B4 Ch05 OK

--- ch04/scripts/gallery.js
+++ ch04/scripts/gallery.js
@@ -2,4 +2,19 @@ function showPic(whichpic) {
     var source = whichpic.getAttribute('href');
     var placeholder = document.getElementById('placeholder');
     placeholder.setAttribute('src', source);
+    var title = whichpic.getAttribute('title');
+    var description = document.getElementById('description');
+    description.firstChild.nodeValue = title;
 }
+
+function countBodyChildren() {
+    var body_element = document.getElementsByTagName('body')[0];
+    // alert(body_element.nodeType);
+    var body_nodes = body_element.childNodes;
+    for (var i = 0; i < body_nodes.length; i++) {
+        var item = body_nodes[i];
+        console.log(item.nodeName, item.nodeType, item.nodeValue);
+    }
+}
+
+window.onload = countBodyChildren;
